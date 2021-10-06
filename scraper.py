8e78c6933590cda75746d1bf8baac1481cda3efa
ben <bpneilsen@gmail.com>
2021-10-06 19:48:26 -0400

    added cookie auth in json, cookie saving/loading

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import os
 from math import ceil
 from urllib.parse import urlparse, parse_qs
+import json
 
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
@@ -9,33 +10,64 @@ from dotenv import load_dotenv
 # Constants, environs
 load_dotenv()
 
+
+class AuthenticationError(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
 class WITSession(requests.Session):
     URL = "https://wits.williamsvillek12.org"
     HOME = URL + "/data/WITS"
     AUTH = URL + "/data/AuthenticateLogin"
     MAIL = URL + "/data/WITSMail?folder=inbox&page=%d"
     CLASSES = URL + "/data/ViewStudentClasses"
-    
+    ASSIGNMENTS = URL + "/data/ClassAssignments"   
+ 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.cookie_authenticate()
 
-        self._authenticate()
-
-    def _authenticate(self):
-        """Authenticates the session using SCHOOL_USER, SCHOOL_PASS"""
+    def authenticate(self, load_homepage=True):
+        """Authenticates the session using SCHOOL_USER and SCHOOL_PASS environ variables"""
         data = {
             "username": os.getenv("SCHOOL_USER"),
             "password": os.getenv("SCHOOL_PASS"),
         }
         resp = self.post(WITSession.AUTH, data=data)
-        if not resp.json().get("data", {}).get("authenticated"):
-            raise ValueError("Credentials are not correct (or WITS isn't serving?)")
+        if not resp.json()["data"]["authenticated"]:     
+            raise AuthenticationError("Credentials not correct, or serverside issue")
         
-        # I don't know why this needs to be done, but if it isn't, any subsequent GETs will redirect here anyways. wtf WITS?
-        self.get(WITSession.HOME)
+        self._save_cookies()
 
+        # If this GET is not made, then the next GET is made here anyways.
+        if load_homepage:
+            self.get(WITSession.HOME)
+
+    def _save_cookies(self, fp="cookies.json"):
+        """Saves the current session's cookies inside a pickle file."""
+        with open(fp, "w") as f:
+            json.dump(self.cookies.get_dict(), f)
+
+    def _load_cookies(self, fp="cookies.json"):
+        """Loads the current session's cookies from a cookie file."""
+        with open(fp) as f:
+            self.cookies = requests.cookies.cookiejar_from_dict(json.load(f))
+
+    def cookie_authenticate(self):
+        """Preferred way of authentication, will try to use a cookie in-place."""
+        try:
+            self._load_cookies()
+            resp = self.get(WITSession.HOME)
+            for script_tag in BeautifulSoup(resp.content, features="html.parser", parse_only=SoupStrainer("script")):
+                if "login" in script_tag.get("src", ""):
+                    self.authenticate()
+                    break
+        except FileNotFoundError:
+            self.authenticate()
+    
     def get_mail(self, num=100):
-        """Obtain mail ids in WITSmail"""
+        """Obtain most recent mail ids from WITSmail"""
         mail_ids = []
 
         assert num > 0, "must get some mail"
@@ -44,13 +76,13 @@ class WITSession(requests.Session):
             resp = self.get(WITSession.MAIL % page)
             # This parses the WITSmail page, then iterates through all links, filters the non-mail links, and gets the ID from each link.
             for link in BeautifulSoup(resp.content, features="html.parser", parse_only=SoupStrainer("a")):
-                if link.has_attr("href") and link["href"].startswith("ViewMessage"):
+                if link.get("href", "").startswith("ViewMessage"):
                     queries = parse_qs(urlparse(link["href"]).query)
                     mail_ids.append(queries["wits_mail_id"][0])
                     num -= 1
                     if num == 0:
                         break
-        return mail
+        return mail_ids
 
     def new_mail(self):
         """Get new WITSmail, since last checked"""
@@ -67,24 +99,17 @@ class WITSession(requests.Session):
         with open("mail_ids.txt", "w") as f:
             f.write("\n".join(mail))
 
-        difference = set(mail) - set(last_read_mail)
-        return difference
+        return set(mail) - set(last_read_mail)
 
     def get_classes(self):
         """Obtain a list of the current classes."""
         resp = self.get(WITSession.CLASSES)
-            
+         
         classes = [
             link["href"] for link in BeautifulSoup(resp.content, features="html.parser", parse_only=SoupStrainer("a"))
-            if link.has_attr("href") and link["href"].startswith("ViewClassNotes")
+            if link.get("href", "").startswith("ViewClassNotes")
         ]
         return classes
 
     def get_notes(self, class_):
-        """Obtain a list of the notes for a class"""
-         
-# TODO: SAVE TOKEN AND CHECK IF STALE (same as storing credentials in untracked file, but skips auth)
-# TODO: ADD OPTION TO RUN WITHOUT SENDING ANYTHING (or if it exceeds a point, just tell them how many they received)
-
-if __name__ == "__main__":
-    session = WITSession()
+        """Obtain a list of the notes for a class""" 
